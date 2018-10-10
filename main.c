51bfae78933208a296a469ce2744193410a34135
lengocho <lengocho2604@gmail.com>
2018-10-10 21:22:12 +0700

    chinh sua lai

--- main.c
+++ main.c
@@ -2,36 +2,40 @@
 #include <stdlib.h>
 
 float tinhluongthang13(int a, int b) {
-    float luongchinhthuc;
-    return (luongchinhthuc);
-
+    float luongchinhthuc, luongthuongthang13;
+    if ((a < 2) && (b < 1)) {
+        luongchinhthuc = 10;
+        luongthuongthang13 = (luongchinhthuc * 30) / 100;
+    }
+    if ((a < 2) && (b > 1) && (b < 2)) {
+        luongchinhthuc = 10;
+        luongthuongthang13 = (luongchinhthuc * 50) / 100;
+    }
+    if ((a > 5) && (b >5)) {
+        luongchinhthuc = 30;
+        luongthuongthang13 = (luongchinhthuc * 2);
+    }
+    return (luongthuongthang13);
 }
-
-
 int main() {
     int a, b;
     printf("Vui long nhap vao so nam kinh nghiem: ");
     scanf("%d", &a);
     printf("Vui long nhap vao so nam cong hien: ");
     scanf("%d", &b);
-    float luongchinhthuc,luongthuongthang13;
     if ((a < 2) && (b < 1)) {
-        luongchinhthuc = 10;
-        tinhluongthang13(a,b) == (luongchinhthuc * 30) / 100;
+
+        tinhluongthang13(a,b);
     }
     if ((a < 2) && (b > 1) && (b < 2)) {
-        luongchinhthuc = 10;
-        luongthuongthang13 = (luongchinhthuc * 50) / 100;
+        tinhluongthang13(a,b);
     }
     if ((a > 2) && (a < 5) && (b > 2) && (b < 5)) {
-        luongchinhthuc = 20;
-        luongthuongthang13 = luongchinhthuc;
+        tinhluongthang13(a,b);
     }
     if ((a > 5) && (b >5)) {
-        luongchinhthuc = 30;
-        luongthuongthang13 = (luongchinhthuc * 2);
+        tinhluongthang13(a,b);
     }
         printf("Luong thuong thang 13 se la %2f trieu", tinhluongthang13(a, b));
-
     return 0;
 }
